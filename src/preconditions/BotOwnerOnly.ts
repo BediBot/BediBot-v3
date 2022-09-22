011824ad52e28ec85696f0d62757d76a42f85470
Zayd Tahir <zaydtahir20@gmail.com>
2022-09-22 17:43:47 -0400

    Removing v2 Files

--- src/preconditions/BotOwnerOnly.ts
+++
@@ -1,9 +0,0 @@
-import {Precondition} from '@sapphire/framework';
-import {Message} from 'discord.js';
-
-export class BotOwnerPrecondition extends Precondition {
-    public run(message: Message) {
-        const BOT_OWNERS = process.env.BOT_OWNERS!.split(',');
-        return BOT_OWNERS.includes(message.author.id) ? this.ok() : this.error({context: {silent: true}});
-    }
-}
