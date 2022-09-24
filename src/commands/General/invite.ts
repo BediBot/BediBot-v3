2e1fd8ddac16590a3c9016fe434dbe20f6aff11d
Zayd Tahir <zaydtahir20@gmail.com>
2022-09-24 16:06:05 -0400

    Invite Slash Command

--- src/commands/General/invite.ts
+++ src/commands/General/invite.ts
@@ -1,10 +1,21 @@
-import {PieceContext} from '@sapphire/framework';
+import {Command, PieceContext} from '@sapphire/framework';
 import {reply} from '@sapphire/plugin-editable-commands';
-import {Formatters, Message, MessageActionRow, MessageButton} from 'discord.js';
+import {CommandInteraction, Formatters, Message, MessageActionRow, MessageButton} from 'discord.js';
 
 import {BediEmbed} from '../../lib/BediEmbed';
 
-const {Command} = require('@sapphire/framework');
+const embed =
+    new BediEmbed()
+        .setTitle('Invite Reply')
+        .setDescription(
+            `Click the link below to invite BediBot to your own server!` +
+            `\n\nEnsure that you have a ${Formatters.inlineCode('SYSTEM MESSAGES CHANNEL')} enabled!` +
+            `\n\nThis can be found under ${Formatters.inlineCode('Server Settings -> Overview -> System Messages Channel')}`);
+
+const row = new MessageActionRow().addComponents(
+    new MessageButton().setLabel('Invite').setStyle('LINK').setURL(
+        'https://discord.com/oauth2/authorize?client_id=873657761391587429&permissions=8&scope=bot%20applications.commands'),
+);
 
 module.exports = class InviteCommand extends Command {
     constructor(context: PieceContext) {
@@ -16,20 +27,19 @@ module.exports = class InviteCommand extends Command {
         });
     }
 
+    public override registerApplicationCommands(registry: Command.Registry) {
+        registry.registerChatInputCommand({name: this.name, description: this.description});
+    }
+
+    async chatInputRun(interaction: CommandInteraction) {
+        return interaction.reply({
+            embeds: [embed],
+            components: [row],
+            ephemeral: true,
+        });
+    }
+
     async messageRun(message: Message) {
-        const embed = new BediEmbed()
-                          .setTitle('Invite Reply')
-                          .setDescription(
-                              `Click the link below to invite BediBot to your own server!` +
-                              `\n\nEnsure that you have a ${Formatters.inlineCode('SYSTEM MESSAGES CHANNEL')} enabled!` +
-                              `\n\nThis can be found under ${
-                                  Formatters.inlineCode('Server Settings -> Overview -> System Messages Channel')}`);
-
-        const row = new MessageActionRow().addComponents(
-            new MessageButton().setLabel('Invite').setStyle('LINK').setURL(
-                'https://discord.com/oauth2/authorize?client_id=873657761391587429&permissions=8&scope=bot%20applications.commands'),
-        );
-
         return reply(message, {
             embeds: [embed],
             components: [row],
