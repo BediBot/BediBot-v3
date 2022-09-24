622c04173d891f28a820801d6501b0978880ee1c
Zayd Tahir <zaydtahir20@gmail.com>
2022-09-24 16:06:09 -0400

    Formatting

--- src/tests/database/BirthdayModel.test.ts
+++ src/tests/database/BirthdayModel.test.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+
 import birthdayModel, {getBirthdaysFromMonth, getBirthdaysToday, updateBirthday} from '../../database/models/BirthdayModel';
 import logger from '../../utils/loggerUtil';
 
