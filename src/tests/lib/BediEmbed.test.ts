b9badabca1c943b12a4f29464d824ad7306916bc
Zayd Tahir <zaydtahir20@gmail.com>
2022-09-23 21:51:35 -0400

    Temporarily Removing Tests

--- src/tests/lib/BediEmbed.test.ts
+++
@@ -1,9 +0,0 @@
-import {BediEmbed} from '../../lib/BediEmbed';
-
-describe('BediEmbed', () => {
-    test('BediEmbed has Correct Properties', () => {
-        const embed = new BediEmbed();
-
-        expect(embed).toBeDefined();
-    });
-});
