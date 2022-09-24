74fd9f11787bb478cb7a01ac698bcdbe5b5193db
Zayd Tahir <zaydtahir20@gmail.com>
2022-09-24 16:40:36 -0400

    Fixing Preconditions

--- src/preconditions/AdminOnly.ts
+++ src/preconditions/AdminOnly.ts
@@ -1,9 +1,22 @@
-import {Precondition} from '@sapphire/framework';
-import {Message, Permissions} from 'discord.js';
+import {Command, Precondition} from '@sapphire/framework';
+import {ContextMenuInteraction, Message, Permissions} from 'discord.js';
+
+import ChatInputInteraction = Command.ChatInputInteraction;
 
 module.exports = class AdminPrecondition extends Precondition {
     public messageRun(message: Message) {
         if (message.guild && message.member!.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) return this.ok();
         return this.error({message: 'Only an admin is allowed to execute this command.'});
+    }
+
+    public chatInputRun(interaction: ChatInputInteraction) {
+        if (interaction.guild && interaction.memberPermissions!.has(Permissions.FLAGS.ADMINISTRATOR)) return this.ok();
+        return this.error({message: 'Only an admin is allowed to execute this command.'});
+    }
+}
+
+declare module '@sapphire/framework' {
+    interface Preconditions {
+        AdminOnly: never;
     }
 }
