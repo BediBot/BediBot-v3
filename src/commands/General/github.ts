b2111a4510f2aea6d094f865bf2f1b24dd29d486
Zayd Tahir <zaydtahir20@gmail.com>
2022-09-24 16:01:38 -0400

    Add Github Slash Command

--- src/commands/General/github.ts
+++ src/commands/General/github.ts
@@ -1,10 +1,18 @@
-import {PieceContext} from '@sapphire/framework';
+import {Command, PieceContext} from '@sapphire/framework';
 import {reply} from '@sapphire/plugin-editable-commands';
 import {Message, MessageActionRow, MessageButton} from 'discord.js';
 
 import {BediEmbed} from '../../lib/BediEmbed';
 
-const {Command} = require('@sapphire/framework');
+const embed =
+    new BediEmbed()
+        .setTitle('GitHub Reply')
+        .setDescription(
+            'BediBot is an open source project managed by Tron 2025s. If you would like to contribute (or star!), head over to our repository.');
+
+const row = new MessageActionRow().addComponents(
+    new MessageButton().setLabel('GitHub').setStyle('LINK').setURL('https://github.com/BediBot/BediBot-v3'),
+);
 
 module.exports = class GithubCommand extends Command {
     constructor(context: PieceContext) {
@@ -16,17 +24,19 @@ module.exports = class GithubCommand extends Command {
         });
     }
 
+    public override registerApplicationCommands(registry: Command.Registry) {
+        registry.registerChatInputCommand({name: this.name, description: this.description});
+    }
+
+    async chatInputRun(interaction: Command.ChatInputInteraction) {
+        return interaction.reply({
+            embeds: [embed],
+            components: [row],
+            ephemeral: true,
+        });
+    }
+
     async messageRun(message: Message) {
-        const embed =
-            new BediEmbed()
-                .setTitle('GitHub Reply')
-                .setDescription(
-                    'BediBot is an open source project managed by Tron 2025s. If you would like to contribute (or star!), head over to our repository.');
-
-        const row = new MessageActionRow().addComponents(
-            new MessageButton().setLabel('GitHub').setStyle('LINK').setURL('https://github.com/BediBot/BediBot-Rewrite'),
-        );
-
         return reply(message, {
             embeds: [embed],
             components: [row],
